431b33984161329c9b230caa484d159c69bae3e4
tomliddle <tom.liddle@gmail.com>
2017-01-06 12:39:39 +0000

    Adds autoapply for evolutions (evolutions wasn't working everytime)

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -1,7 +1,11 @@
+#Schema
+
 # --- !Ups
+
 create table "stocks" ("id" VARCHAR(254) NOT NULL PRIMARY KEY, "name" VARCHAR(254) NOT NULL);
 create table "quotes" ("ticker" VARCHAR(254) NOT NULL,"price" DECIMAL(10,3) NOT NULL, "datetime" TIMESTAMP NOT NULL);
 
 # --- !Downs
+
 drop table "stocks";
 drop table "quotes";
